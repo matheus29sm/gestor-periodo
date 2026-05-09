135967712c187f73eaafcc5a50679a02b2e17786
matheus29sm <matheus05sm@gmail.com>
2026-05-09 19:46:59 -0300

    feat: Adicionar novo retorno detalhado ao período DTO.

--- src/main/java/com/matheus/gestor_periodo/dto/periodo/PeriodoReponseDTO.java
+++ src/main/java/com/matheus/gestor_periodo/dto/periodo/PeriodoReponseDTO.java
@@ -1,12 +1,28 @@
 package com.matheus.gestor_periodo.dto.periodo;
 
+import com.matheus.gestor_periodo.dto.diasSemana.DiaSemanaResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public class PeriodoReponseDTO {
 
     public interface Periodo {
         LocalDate getDataInicial();
         LocalDate getDataFinal();
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PeriodoDetalhado {
+        private String dataInicial;
+        private String dataFinal;
+        private Long totalDias;
+        private List<DiaSemanaResponseDTO.DiaSemana> distribuicaoDiasSemana;
     }
 
 }
